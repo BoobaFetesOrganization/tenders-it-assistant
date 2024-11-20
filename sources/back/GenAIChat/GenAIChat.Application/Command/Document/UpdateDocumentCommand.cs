120bac05c6c88bbe636b6f9038e935d34ef9a543
Axel RG <rg_axel@yahoo.fr>
2024-11-20 18:50:58 +0100

    check si un fichier a le meme nom avant mise à jour du document

--- sources/back/GenAIChat/GenAIChat.Application/Command/Document/UpdateDocumentCommand.cs
+++ sources/back/GenAIChat/GenAIChat.Application/Command/Document/UpdateDocumentCommand.cs
@@ -1,6 +1,7 @@
 ﻿using GenAIChat.Application.Adapter.Api;
 using GenAIChat.Application.Adapter.Database;
 using GenAIChat.Application.Command.Common;
+using GenAIChat.Domain.Common;
 using GenAIChat.Domain.Document;
 using MediatR;
 
@@ -12,6 +13,12 @@ namespace GenAIChat.Application.Command.Document
         {
             var document = await unitOfWork.Documents.GetByIdAsync(request.Entity.Id);
             if (document is null) return null;
+
+            var isExisting = (await unitOfWork.Documents.GetAllAsync(PaginationOptions.All,
+                p => p.ProjectId == request.Entity.ProjectId
+                && p.Name.ToLower().Equals(request.Entity.Name.ToLower()))
+                ).Any();
+            if (isExisting) throw new Exception("Document with the same name already exists for the project");
 
             document.ProjectId = request.Entity.ProjectId;
             document.Name = request.Entity.Name;
