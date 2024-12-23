db7e91f38b25aa74f7c9d5e6d4a08a9d877b0b8f
Axel RG <rg_axel@yahoo.fr>
2024-12-23 12:32:28 +0100

    remove useless code

--- sources/front/apps/client/src/app/HeaderBar/SearchAppBar/SearchAppBar.tsx
+++ sources/front/apps/client/src/app/HeaderBar/SearchAppBar/SearchAppBar.tsx
@@ -1,68 +1,12 @@
 //import { Search } from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Input } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import { alpha, styled } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { FC, memo, MouseEvent, useCallback, useState } from 'react';
 import { AppBarMenu } from '../menus';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const DeleteFilterIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 1, 0, 1),
-  height: '100%',
-  position: 'absolute',
-  right: 0,
-  cursor: 'pointer',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-}));
-
-const StyledInput = styled(Input)(({ theme }) => ({
-  color: 'inherit',
-  width: `100%`,
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      margin: theme.spacing(0, 3, 0, 0),
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 interface ISearchAppBarProps {
   title: string;
@@ -98,21 +42,6 @@ export const SearchAppBar: FC<ISearchAppBarProps> = memo(({ title }) => {
           >
             {title}
           </Typography>
-          {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInput
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              endAdornment={
-                <DeleteFilterIconWrapper onClick={() => setFilter(null)}>
-                  <CloseIcon />
-                </DeleteFilterIconWrapper>
-              }
-              onChange={(e) => setFilter(e.target.value.toLowerCase())}
-            />
-          </Search> */}
         </Toolbar>
       </AppBar>
       <AppBarMenu anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
