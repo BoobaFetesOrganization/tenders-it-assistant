47f486fc754c570c38595d2f648637aa60b8fdb4
Axel RG <rg_axel@yahoo.fr>
2025-02-11 17:56:00 +0100

    feat-add-TableStorage-database

---
+++ src/back/GenAIChat/GenAIChat.Infrastructure.Database.TableStorage/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace GenAIChat.Infrastructure.Database.TableStorage
+{
+    public class Class1
+    {
+
+    }
+}
+az storage account create -n mystorageaccount -g MyResourceGroup -l westus --subscription MySubscription
