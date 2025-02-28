0f32506561f60ba5adfe9433ca71f83e53d181b2
Axel RG <rg_axel@yahoo.fr>
2025-02-28 10:32:19 +0100

    remove warnings

--- src/back/GenAIChat/GenAIChat.Application/Usecase/AppplicationBase.cs
+++ src/back/GenAIChat/GenAIChat.Application/Usecase/AppplicationBase.cs
@@ -18,18 +18,18 @@ namespace GenAIChat.Application.Usecase
         }
 
         public async Task<Paged<TDomain>> GetAllPagedAsync(PaginationOptions options, IFilter? filter = null, CancellationToken cancellationToken = default)
-            => await mediator.Send(new GetAllPagedQuery<TDomain> { Options = options, Filter = filter }, cancellationToken = default);
+            => await mediator.Send(new GetAllPagedQuery<TDomain> { Options = options, Filter = filter }, cancellationToken);
 
         public async Task<TDomain?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
-            => await mediator.Send(new GetByIdQuery<TDomain> { Id = id }, cancellationToken = default);
+            => await mediator.Send(new GetByIdQuery<TDomain> { Id = id }, cancellationToken);
 
         public async virtual Task<TDomain> CreateAsync(TDomain domain, CancellationToken cancellationToken = default)
-            => await mediator.Send(new CreateCommand<TDomain> { Domain = domain }, cancellationToken = default);
+            => await mediator.Send(new CreateCommand<TDomain> { Domain = domain }, cancellationToken);
 
         public async virtual Task<bool?> UpdateAsync(TDomain domain, CancellationToken cancellationToken = default)
-            => await mediator.Send(new UpdateCommand<TDomain> { Domain = domain }, cancellationToken = default);
+            => await mediator.Send(new UpdateCommand<TDomain> { Domain = domain }, cancellationToken);
 
         public async Task<bool?> DeleteAsync(string id, CancellationToken cancellationToken = default)
-            => await mediator.Send(new DeleteByIdCommand<TDomain> { Id = id }, cancellationToken = default);
+            => await mediator.Send(new DeleteByIdCommand<TDomain> { Id = id }, cancellationToken);
     }
 }
