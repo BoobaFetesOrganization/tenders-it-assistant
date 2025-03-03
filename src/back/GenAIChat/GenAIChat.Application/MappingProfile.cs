2d74e14b5548b67830a5918f43598eff1c5e6991
Axel RG <rg_axel@yahoo.fr>
2025-03-03 18:29:06 +0100

    ajout des map pour automapper pour pouvoir effectuer des clonages des objets du domaine

---
+++ src/back/GenAIChat/GenAIChat.Application/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GenAIChat.Domain.Common;
+using GenAIChat.Domain.Document;
+using GenAIChat.Domain.Project;
+using GenAIChat.Domain.Project.Group;
+using GenAIChat.Domain.Project.Group.UserStory;
+using GenAIChat.Domain.Project.Group.UserStory.Task;
+using GenAIChat.Domain.Project.Group.UserStory.Task.Cost;
+
+namespace GenAIChat.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // enable clone with Automapper
+
+            CreateMap<EntityDomain, EntityDomain>();
+            CreateMap<ProjectDomain, ProjectDomain>();
+            CreateMap<DocumentDomain, DocumentDomain>();
+            CreateMap<DocumentMetadataDomain, DocumentMetadataDomain>();
+            CreateMap<UserStoryRequestDomain, UserStoryRequestDomain>();
+            CreateMap<UserStoryDomain, UserStoryDomain>();
+            CreateMap<TaskDomain, TaskDomain>();
+            CreateMap<TaskCostDomain, TaskCostDomain>();
+        }
+    }
+}
