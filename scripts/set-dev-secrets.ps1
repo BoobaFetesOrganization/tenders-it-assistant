a5bb23f903a5070aaf7131015792781d2ed40590
Axel RG <rg_axel@yahoo.fr>
2025-03-19 18:57:09 +0100

    script d'installation du poste de devdev milse à jour

--- scripts/set-dev-secrets.ps1
+++ scripts/set-dev-secrets.ps1
@@ -8,8 +8,73 @@ param (
 $ErrorActionPreference = "Stop"
 $projectRoot = ([DirectoryInfo](Join-Path $PSScriptRoot "..")).FullName
 
-if (-not $secrets) {
-    Write-Host "Usage: set-dev-secrets.ps1 -secrets @{ connectionstring= 'your_connection_string'; apikey = 'your_api_key' }"
+# Liste des clés de secrets requises
+$requiredSecrets = @(
+    "AZURE_STORAGE_CONNECTIONSTRING",
+    "GOOGLE_GEMINI_APIKEY",
+    "AZURE_TENANT_ID",
+    "AZURE_WEBAPP_LOG_ID",
+    "AZURE_WEBAPP_LOG_SECRET"
+)
+# Fonction pour vérifier la validité d'un secret
+function Test-SecretValidity {
+    param (
+        [string]$secretName,
+        [string]$secretValue
+    )
+
+    if ($null -eq $secretValue) {
+        Write-Error "Le secret '$secretName' est invalide : valeur nulle."
+        return $false
+    }
+    elseif ("" -eq $secretValue) {
+        Write-Error "Le secret '$secretName' est invalide : valeur vide."
+        return $false
+    }
+    elseif ($secretValue -match '\s') {
+        Write-Error "Le secret '$secretName' est invalide : valeur ne contenant que des espaces ou tabulations."
+        return $false
+    }
+    
+    return $true
+}
+
+# affichage du paramètre attendu
+Write-Host 'pour rappel, le parametre $secrets est un objet qui est defini comme ci-dessous' -ForegroundColor Yellow
+Write-Host "(le paramètre 'secret' n'est pas obligé de contenir toutes les propriétés listées)" -ForegroundColor Yellow
+Write-Host '$secrets = @{' -ForegroundColor Yellow
+$requiredSecrets | ForEach-Object { Write-Host "  $_ = ""the secret""" -ForegroundColor Yellow }
+Write-Host '}' -ForegroundColor Yellow
+Write-Host './set-dev-secrets.ps1' -ForegroundColor Yellow
+Write-Host
+
+if ($null -eq $secrets) {
+    Write-Error "Le paramètre 'secrets' est nul. Veuillez le définir."
+    exit
+}
+elseif ($secrets.GetType().Name -ne "Hashtable") {
+    Write-Error "Le paramètre 'secrets' n'est pas un Hashtable. Veuillez le définir."
+    exit
+}
+$allSecretsValid = $true
+foreach ($secretName in $requiredSecrets) {
+    if ($secrets.ContainsKey($secretName)) {
+        $secretValue = $secrets[$secretName]
+        if (-not (Test-SecretValidity -secretName $secretName -secretValue $secretValue)) {
+            $allSecretsValid = $false
+        }
+    }
+}
+if ($allSecretsValid -and $secrets.Count -eq 0) {
+    Write-Error "Aucune propriété n'est définie dans \$secrets."
+    $allSecretsValid = $false
+}
+# Affichage d'un message si tous les secrets sont valides
+if ($allSecretsValid) {
+    Write-Host "Tous les secrets saisis sont valides."
+}
+else {
+    Write-Error "Certains secrets sont invalides. Veuillez les corriger."
     exit
 }
 
@@ -19,32 +84,52 @@ if (-not $projectPath.Exists) {
     exit
 }
 
+$location = Get-Location
+try {
+    Set-Location $projectPath
 
-
-# Initialize user-secrets if not already done
-[xml]$csprojContent = Get-Content "$($projectPath)/TendersITAssistant.Presentation.API.csproj"
-if (-not $csprojContent.Project.PropertyGroup.UserSecretsId) {
-    Write-Host "prepare project '$($projectPath.FullName)' to receive secrets" -ForegroundColor Yellow
-    dotnet user-secrets init --project $projectPath
-}
-else {
-    Write-Host "project '$($projectPath.FullName)' is already set up to use secrets" -ForegroundColor Yellow
-    Write-Host "list of current secrets" -ForegroundColor Yellow
-    dotnet user-secrets list --project $projectPath
-    Write-Host "end of the current secrets" -ForegroundColor Yellow
-
-    if ($clear) {
-        Write-Host "clear current secrets" -ForegroundColor Red
-        dotnet user-secrets clear --project $projectPath
+    # Initialize user-secrets if not already done
+    [xml]$csprojContent = Get-Content "$($projectPath)/TendersITAssistant.Presentation.API.csproj"
+    if (-not $csprojContent.Project.PropertyGroup.UserSecretsId) {
+        Write-Host "prepare project '$($projectPath.FullName)' to receive secrets" -ForegroundColor Yellow
+        dotnet user-secrets init
     }
+    else {
+        Write-Host "project '$($projectPath.FullName)' is already set up to use secrets" -ForegroundColor Yellow
+        Write-Host "list of before update:"
+        dotnet user-secrets list
+    }
+    Write-Host
+
+    # Add secrets
+    if ($clear) {
+        Write-Host 'clear existing secrets'
+        dotnet user-secrets clear
+        Write-Host
+    }
+    
+    write-host 'set new secrets'
+    if ($secrets.ContainsKey("AZURE_STORAGE_CONNECTIONSTRING")) {
+        dotnet user-secrets set "ConnectionStrings:accountstorage" "$($secrets.AZURE_STORAGE_CONNECTIONSTRING)"
+    }
+    if ($secrets.ContainsKey("GOOGLE_GEMINI_APIKEY")) {
+        dotnet user-secrets set "AI:Gemini_ApiKey" "$($secrets.GOOGLE_GEMINI_APIKEY)"    
+    }
+    if ($secrets.ContainsKey("AZURE_TENANT_ID")) {
+        dotnet user-secrets set "Serilog:WriteTo:AzureLogAnalytics:Args:credentials:tenantId" "$($secrets.AZURE_TENANT_ID)"
+    }
+    if ($secrets.ContainsKey("AZURE_WEBAPP_LOG_ID")) {
+        dotnet user-secrets set "Serilog:WriteTo:AzureLogAnalytics:Args:credentials:clientId" "$($secrets.AZURE_WEBAPP_LOG_ID)"
+    }
+    if ($secrets.ContainsKey("AZURE_WEBAPP_LOG_SECRET")) {
+        dotnet user-secrets set "Serilog:WriteTo:AzureLogAnalytics:Args:credentials:clientSecret" "$($secrets.AZURE_WEBAPP_LOG_SECRET)"
+    }
+    Write-Host
+
+    # display current secrets
+    Write-Host "list of current secrets:" -ForegroundColor Yellow
+    dotnet user-secrets list
 }
-
-# Add secrets
-dotnet user-secrets set "ConnectionStrings:accountstorage" $secrets.connectionstring --project $projectPath
-dotnet user-secrets set "AI:Gemini_ApiKey" $secrets.apikey --project $projectPath
-
-# display current secrets
-Write-Host "list of new secrets" -ForegroundColor Yellow
-dotnet user-secrets list --project $projectPath
-Write-Host "end of the new secrets" -ForegroundColor Yellow
-
+finally {
+    Set-Location $location
+}
