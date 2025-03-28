009b1edb5d095b3f54397fc5c0264c43ff0ae686
Axel Roland-Gosselin <41676042+BoobaFetes@users.noreply.github.com>
2025-03-28 13:00:54 +0100

    feat-create-sp-for-many-scopes (#21)
    * create sp for many scopes
    
    * undo url rewriting

--- azure/lib/resources-lib-azure.ps1
+++ azure/lib/resources-lib-azure.ps1
@@ -38,11 +38,11 @@ function Invoke-Az-Command(
         # .. or contains a false error message from az cli extensions
         $hasWarning = { param([string]$content)
             $falseErrors = @()
-            $falseErrors += $content -match "(?s)^az.*azure.*cliextensions.*invalid escape.*Create a data collection rule\.\r\n$"  
+            $falseErrors += $content -match "(?s)^az.*azure.*cliextensions.*invalid.*escape.*sequence.*Create a data collection rule\.\r\n$"  
             $falseErrors += $content -match "(?s)^a.*UserWarning.*cryptography.*NativeCommandError.*\r\n$"   
             $falseErrors += $content -match "(?s)^.*UserWarning.*cryptography.*resources-create.ps1.*\r\n$"   
             $falseErrors += $content -match "(?s)^az : WARNING.*The output includes credentials that you must protect.*https://aka.ms/azadsp-cli\r\n$"
-            return ($falseErrors | Where-Object { $_ -eq $true }).Count -gt 0
+            return($falseErrors | Where-Object { $_ -eq $true }).Count -gt 0
         }
         if ($hasError -and $hasWarning.invoke($content)[0]) {
             $hasError = $false
@@ -79,38 +79,4 @@ function Get-Tags-AsKeyValue-ToString(
     [object[]] $tags
 ) {
     return (Get-Tags-AsKeyValue $tags) -join " "
-}
-
-function Get-ServicePrincipal(
-    [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
-    [object] $sp
-) {
-    return (az ad sp list --filter "displayname eq '$($sp.name)'" | ConvertFrom-Json)[0]
-}
-
-function Set-ServicePrincipal(
-    [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
-    [object] $sp,
-    [Parameter(Mandatory = $true)]
-    [Hashtable] $references,  
-    [Parameter(Mandatory = $true)]
-    [string] $scopes
-) {
-    $result = $sp | Get-ServicePrincipal
-    if ($null -eq $result) {
-        $cmd = "az ad sp create-for-rbac"
-        $cmd += " -n $($sp.name)"
-        $cmd += " --role ""$($sp.role)"""
-        $cmd += " --scopes $scopes"
-        $servicePrincipal = $cmd | Invoke-Az-Command -name $sp.name -ErrorFile $ErrorFile        
-        $servicePrincipal | New-Resource-File-Service-Principal
-    }
-    else {
-        Write-Host "service principal '$($result.appDisplayName)' already exists" -ForegroundColor Yellow
-        $spFile = $sp.name | Get-Secret-File
-        if (-not $spFile.Exists) { throw "service principal '$($result.appDisplayName)' already exists but the secret file is missing" }
-        $servicePrincipal = Get-Content -Path $spFile.FullName -Raw | ConvertFrom-Json
-    }
-
-    $references[$sp.name] = $servicePrincipal
 }
